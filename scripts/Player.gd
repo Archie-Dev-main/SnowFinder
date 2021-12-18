4655f7db8bd71b1c4cc4d3f593ef8f8a076e0bed
Archie-Dev-main <80728942+Archie-Dev-main@users.noreply.github.com>
2021-12-17 18:01:37 -0600

    Update Player.gd

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -21,7 +21,7 @@ var MOUSE_SENSITIVITY = 0.05
 func _ready():
 	camera = $Rotation_Helper/Camera
 	rotation_helper = $Rotation_Helper
-
+	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
@@ -30,15 +30,14 @@ func _physics_process(delta):
 	process_movement(delta)
 
 
-func process_input(delta):
-
+func process_input(_delta):
 	# ----------------------------------
 	# Walking
 	dir = Vector3()
 	var cam_xform = camera.get_global_transform()
-
+	
 	var input_movement_vector = Vector2()
-
+	
 	if Input.is_action_pressed("movement_forward"):
 		input_movement_vector.y += 1
 	if Input.is_action_pressed("movement_backward"):
@@ -47,21 +46,21 @@ func process_input(delta):
 		input_movement_vector.x -= 1
 	if Input.is_action_pressed("movement_right"):
 		input_movement_vector.x += 1
-
+		
 	input_movement_vector = input_movement_vector.normalized()
-
+	
 	# Basis vectors are already normalized.
 	dir += -cam_xform.basis.z * input_movement_vector.y
 	dir += cam_xform.basis.x * input_movement_vector.x
 	# ----------------------------------
-
+	
 	# ----------------------------------
 	# Jumping
 	if is_on_floor():
 		if Input.is_action_just_pressed("movement_jump"):
 			vel.y = JUMP_SPEED
 	# ----------------------------------
-
+	
 	# ----------------------------------
 	# Capturing/Freeing the cursor
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -75,21 +74,21 @@ func process_input(delta):
 func process_movement(delta):
 	dir.y = 0
 	dir = dir.normalized()
-
+	
 	vel.y += delta * GRAVITY
-
+	
 	var hvel = vel
 	hvel.y = 0
-
+	
 	var target = dir
 	target *= MAX_SPEED
-
+	
 	var accel
 	if dir.dot(hvel) > 0:
 		accel = ACCEL
 	else:
 		accel = DEACCEL
-
+		
 	hvel = hvel.linear_interpolate(target, accel * delta)
 	vel.x = hvel.x
 	vel.z = hvel.z
@@ -100,7 +99,7 @@ func _input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY * -1))
 		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
-
+		
 		var camera_rot = rotation_helper.rotation_degrees
 		camera_rot.x = clamp(camera_rot.x, -70, 70)
 		rotation_helper.rotation_degrees = camera_rot
